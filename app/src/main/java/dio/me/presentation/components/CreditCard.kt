2f3f237b1cb488da7ea116d990f8812bcfced677
Douglas <douglas.159@hotmail.com>
2023-08-05 21:47:32 -0300

    finish CreditCard and NewsPager puting Red Button above the card

--- app/src/main/java/dio/me/presentation/components/CreditCard.kt
+++ app/src/main/java/dio/me/presentation/components/CreditCard.kt
@@ -1,17 +1,61 @@
 package dio.me.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dio.me.presentation.theme.SantanderDevWeekTheme
+import dio.me.presentation.theme.Spacing_2
 
 @Composable
 fun CreditCard(
     modifier: Modifier = Modifier,
     number: String = ""
 ) {
-    AppCard(containerColor = MaterialTheme.colorScheme.primary) {
+    AppCard(
+        modifier
+            .fillMaxWidth()
+            .height(66.dp),
+        containerColor = MaterialTheme.colorScheme.primary
+    ) {
+        Row(
+            modifier
+                .fillMaxSize()
+                .padding(horizontal = Spacing_2),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Card final number",
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.W600,
+                    letterSpacing = 0.18.sp
+                )
+
+            )
+            Icon(
+                imageVector = Icons.Filled.ExpandMore, contentDescription = "ExpandMore",
+
+                )
+        }
 
     }
 
@@ -23,6 +67,6 @@ fun CreditCard(
 @Composable
 fun CreditCardPreview() {
     SantanderDevWeekTheme {
-        CreditCard( number = "0000")
+        CreditCard(number = "0000")
     }
 }
